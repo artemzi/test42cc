ee3e9c7806562730f91c9e56a92494b095322d11
Artem Zinoviev <arzinoviev@gmail.com>
2013-06-10 18:15:47 +0300

    tiket:5 fix settings v2

--- test42cc/conf/base.py
+++ test42cc/conf/base.py
@@ -77,7 +77,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = normpath(join(SITE_ROOT, 'assets'))
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -88,7 +88,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
