6a84625e4b6ef1184361e56b6260960ed931c03e
Artem Zinoviev <arzinoviev@gmail.com>
2013-06-11 17:57:44 +0300

    tiket:5 add photo preview in edit page

--- t5_editform/form.py
+++ t5_editform/form.py
@@ -1,12 +1,18 @@
 from django import forms
+from .widgets import AdvancedFileInput
 
 from t1_contact.models import CustomProfile
+
+
+class PhotoField(forms.ImageField):
+    widget = AdvancedFileInput
 
 
 class EditForm(forms.ModelForm):
     class Meta(object):
         model = CustomProfile
-
+        photo = PhotoField(label=u"Photo")
+        
     def __init__(self, *args, **kwargs):
         super (EditForm, self).__init__(*args, **kwargs)
         self.fields['estate'].queryset = EditForm.objects.filter(estate__user__exact=Estates.objects.get(self._user))
