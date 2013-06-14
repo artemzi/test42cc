d56648658eed0b1bcfb753f8b22ba8d1cf5ec80a
Artem Zinoviev <arzinoviev@gmail.com>
2013-06-14 13:29:23 +0300

    tiket:13 #fix for tiket:6 test that form sends via ajax request

--- t5_editform/tests.py
+++ t5_editform/tests.py
@@ -4,11 +4,12 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 
 class EditFormTest(TestCase):
+    
     def test_that_login_exists(self):
         """
         when you try go to editpage.html in response 
@@ -16,3 +17,10 @@ class EditFormTest(TestCase):
         """
         response = self.client.get('/update/1')
         field_names_in_view = ["Username", "Password"]
+
+    def test_that_request_is_ajax(self):
+        User.objects.create_user('test', 'test@gmail.com', 'test')
+        self.client.login(username='test', password='test')
+        r = self.client.post('/update/1', {'value': '1',},
+                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertTrue(r)
