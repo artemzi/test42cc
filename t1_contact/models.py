09a87cb6e6443472b0953781a7e8d0b62de15f47
Artem Zinoviev <arzinoviev@gmail.com>
2013-06-18 22:18:48 +0300

    tiket:1 fix bug from merge

--- t1_contact/models.py
+++ t1_contact/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Person(models.Model):
+class CustomProfile(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birth_date = models.DateField(verbose_name="Date of birth")
